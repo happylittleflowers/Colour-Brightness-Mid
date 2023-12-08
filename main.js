ee0f950169d7f101cbd559bf0f59c9da077f6cb8
happylittleflowers <145370707+happylittleflowers@users.noreply.github.com>
2023-12-08 09:24:32 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-// LUMINENCE CALCULATOR
+// BRIGHTNESS LUMINENCE CALCULATOR
+// Button Click Listener
+document.getElementById("btn").addEventListener("click", btnClicked);
 
-// INPUT
+function btnClicked() {
+  // INPUT
+  let r = +document.getElementById("r-in").value;
+  let g = +document.getElementById("g-in").value;
+  let b = +document.getElementById("b-in").value;
 
-let R = +prompt("Enter a R color value (Red):");
-let G = +prompt("Enter a G color value (Green):");
-let B = +prompt("Enter a B color value (Blue):");
+  // PROCESS
+  let brightness = Math.sqrt(0.299 * r ** 2 + 0.587 * g ** 2 + 0.114 * b ** 2);
+  let msg = `Luminance = ${brightness}`;
 
-// PROCESS
-let brightness = Math.sqrt(0.299(R) ** 2 + 0.587(G) ** 2 + 0.114(B) ** 2);
+  // OUTPUT
+  document.getElementById("output").innerHTML = Math.round(brightness);
+}
